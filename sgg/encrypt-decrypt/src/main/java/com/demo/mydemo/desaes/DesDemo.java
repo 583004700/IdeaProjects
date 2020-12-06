e1ccdcc61faef826295c42c3dba11f923aac6c8e
583004700 <583004700@qq.com>
2020-12-06 11:27:10 +0800

    encrypt-decrypt

--- sgg/encrypt-decrypt/src/main/java/com/demo/mydemo/desaes/DesDemo.java
+++ sgg/encrypt-decrypt/src/main/java/com/demo/mydemo/desaes/DesDemo.java
@@ -3,17 +3,23 @@ package com.demo.mydemo.desaes;
 import com.sun.org.apache.xml.internal.security.utils.Base64;
 
 import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
 public class DesDemo {
     public static void main(String[] args) throws Exception{
-        //原文
-        String input = "硅谷";
+        //原文  填充模式如果使用NoPadding，那么原文必须是8个字节的整数倍
+        String input = "硅谷12";
         //定义key
         //如果使用des进行加密，那么密钥必须是8个字节
         String key = "12345678";
-        //算法
-        String transformation = "DES";
+        //算法    qANksk5lvqM=
+//        String transformation = "DES";
+        //算法/加密模式/填充模式
+        //如果默认情况下，如果没写填充模式和加密模式，那么默认使用ECB/PKCS5Padding
+//        String transformation = "DES/ECB/PKCS5Padding";
+        String transformation = "DES/CBC/PKCS5Padding";
+//        String transformation = "DES/CBC/NoPadding";
         //加密类型
         String algorithm = "DES";
         //加密
@@ -27,8 +33,12 @@ public class DesDemo {
     public static String decryptDES(String encryptDES,String key,String transformation, String algorithm) throws Exception {
         Cipher cipher = Cipher.getInstance(transformation);
         SecretKeySpec secretKeySpec = new SecretKeySpec(key.getBytes(),algorithm);
+
+        //创建iv向量，iv向量是使用CBC加密模式,iv的字节也是8个
+        IvParameterSpec iv = new IvParameterSpec("87654321".getBytes());
+
         //Cipher.DECRYPT_MODE:解密模式
-        cipher.init(Cipher.DECRYPT_MODE,secretKeySpec);
+        cipher.init(Cipher.DECRYPT_MODE,secretKeySpec,iv);
         byte[] bytes = cipher.doFinal(Base64.decode(encryptDES));
         return new String(bytes);
     }
@@ -41,10 +51,13 @@ public class DesDemo {
         //第一个参数：表示key的字节
         //第二个参数：表示加密类型
         SecretKeySpec secretKeySpec = new SecretKeySpec(key.getBytes(),algorithm);
+        //创建iv向量，iv向量是使用CBC加密模式,iv的字节也是8个
+        IvParameterSpec iv = new IvParameterSpec("87654321".getBytes());
+
         //进行加密初始化
         //第一个参数表示模式，加密模式，解密模式
         //第二个参数表示加密规则
-        cipher.init(Cipher.ENCRYPT_MODE,secretKeySpec);
+        cipher.init(Cipher.ENCRYPT_MODE,secretKeySpec,iv);
         //调用加密方法
         //参数表示原文的字节数组
         byte[] bytes = cipher.doFinal(input.getBytes());
