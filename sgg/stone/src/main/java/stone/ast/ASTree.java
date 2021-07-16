2467ab0d3edef139922fa0ce73e5c1f05fa6a15c
583004700 <583004700@qq.com>
2021-07-16 17:28:23 +0800

    两周自制脚本语言

--- sgg/stone/src/main/java/stone/ast/ASTree.java
+++ sgg/stone/src/main/java/stone/ast/ASTree.java
@@ -2,9 +2,35 @@ package stone.ast;
 import java.util.Iterator;
 
 public abstract class ASTree implements Iterable<ASTree> {
+
+    /**
+     * 获取第i个子节点
+     * @param i
+     * @return
+     */
     public abstract ASTree child(int i);
+
+    /**
+     * 获取子节点数量
+     * @return
+     */
     public abstract int numChildren();
+
+    /**
+     * 得到子节点迭代器
+     * @return
+     */
     public abstract Iterator<ASTree> children();
+
+    /**
+     * 定位行号信息
+     * @return
+     */
     public abstract String location();
+
+    /**
+     * 本身也是可迭代的
+     * @return
+     */
     public Iterator<ASTree> iterator() { return children(); }
 }
--- sgg/stone/src/main/java/stone/ast/BinaryExpr.java
+++ sgg/stone/src/main/java/stone/ast/BinaryExpr.java
@@ -1,6 +1,9 @@
 package stone.ast;
 import java.util.List;
 
+/**
+ * 二元运算符，具有两个操作数
+ */
 public class BinaryExpr extends ASTList {
     public BinaryExpr(List<ASTree> c) { super(c); }
     public ASTree left() { return child(0); }
