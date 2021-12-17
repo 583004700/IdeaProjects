847e4425c10f624d0fe34528431d7f99c6382c09
zhuwb <583004700@qq.com>
2021-12-18 01:20:48 +0800

    ObjectUtils

--- sgg/mybatis/src/main/java/common/utils/ObjectUtils.java
+++ sgg/mybatis/src/main/java/common/utils/ObjectUtils.java
@@ -1,6 +1,8 @@
 package common.utils;
 
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
+
 import ognl.Ognl;
 import ognl.OgnlContext;
 
@@ -30,7 +32,15 @@ public class ObjectUtils {
                     return null;
                 }
                 if (data instanceof JSONObject) {
-                    return ((JSONObject) data).getString(key);
+                    int leftIndex = key.indexOf("[");
+                    if(leftIndex == -1) {
+                        return ((JSONObject) data).get(key);
+                    }else{
+                        int rightIndex = key.indexOf("]");
+                        int index = Integer.valueOf(key.substring(leftIndex+1,rightIndex));
+                        String realKey = key.substring(0,leftIndex);
+                        ((JSONObject) data).getJSONArray(realKey).get(index);
+                    }
                 }
                 if(data instanceof String){
                     String r = (String) data;
@@ -49,8 +59,6 @@ public class ObjectUtils {
     }
 
     public static void main(String[] args) {
-        System.out.println("--------------------");
-
         String jsonStr = "{\"extendField2\":\"{\\\"extendField3\\\":\\\"aaa\\\",\\\"extendField1\\\":\\\"CC2100-总经办\\\"}\",\"extendField1\":\"CC2100-总经办\"}";
         System.out.println("原对象："+jsonStr);
         System.out.println("取extendField2为："+ObjectUtils.getValue(jsonStr,"extendField2"));
@@ -70,5 +78,22 @@ public class ObjectUtils {
         System.out.println(ObjectUtils.getValue(joa.toJSONString(),"a.b.c.d"));
 
         System.out.println("----------------------");
+
+        JSONObject jbd = new JSONObject();
+        JSONArray ja = new JSONArray();
+        ja.add(1);
+        ja.add(2);
+        jbd.put("d",ja);
+        JSONObject jbc = new JSONObject();
+        jbc.put("c",jbd.toJSONString());
+        JSONObject jbb = new JSONObject();
+        jbb.put("b",jbc.toJSONString());
+        JSONObject jba = new JSONObject();
+        jba.put("a",jbb.toJSONString());
+        String jbaStr = jba.toJSONString();
+        System.out.println(jbaStr);
+        System.out.println(ObjectUtils.getValue(jbaStr,"a.b.c.d"));
+        System.out.println(ObjectUtils.getValue(jbaStr,"a.b.c.d[0]"));
+        System.out.println(ObjectUtils.getValue(jbaStr,"a.b.c.d["));
     }
 }
