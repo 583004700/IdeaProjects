8a1d1fa9962f0d262cd1776902d042ec79022e98
zhuwb <583004700@qq.com>
2022-04-28 09:20:35 +0800

    B树的删除

--- book/qxsjjg/src/main/java/chapter10/BTree.java
+++ book/qxsjjg/src/main/java/chapter10/BTree.java
@@ -6,8 +6,11 @@ import lombok.experimental.Accessors;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
+import java.util.Set;
 
 /**
  * B树
@@ -23,7 +26,7 @@ public class BTree<T extends Comparable<T>> {
     }
 
     public void add(T data) {
-        if(search(data) != null){
+        if (search(data) != null) {
             // 节点已经存在
             return;
         }
@@ -46,24 +49,238 @@ public class BTree<T extends Comparable<T>> {
                 pNode.addChildNode(listNode.get(1));
                 tNode = pNode;
                 up = pNode.add(up.getValue());
-                printTree();
             }
-            printTree();
         }
     }
 
-    public T search(T data){
-        return doSearch(root,data);
+    public void delete(T data) {
+        Node<T> tNode = searchNode(data);
+        if (tNode == null) {
+            return;
+        }
+        if (root.getDataList().size() == 1 && root.getChildrenList().size() == 0) {
+            root = null;
+            return;
+        }
+        if (root.getChildrenList().size() == 0) {
+            root.removeData(data);
+            return;
+        }
+        if (tNode.getLeftNode() != null) {
+            Node<T> leftMax = getMax(tNode.getLeftNode(data));
+            T leftMaxData = leftMax.getMax();
+            tNode.removeData(data);
+            tNode.add(leftMaxData);
+            if (leftMax.getDataList().size() == 1) {
+                down(leftMax);
+            }
+            leftMax.removeData(leftMaxData);
+        } else {
+            if (tNode.getDataList().size() == 1) {
+                down(tNode);
+            }
+            tNode.removeData(data);
+        }
+    }
+
+    // 下溢
+    public void down(Node<T> node) {
+        Node<T> leftBrother = getLeftBrother(node);
+        Node<T> rightBrother = getRightBrother(node);
+        Node<T> parent = searchParentNode(node);
+
+        Node<T> borrow = leftBrother != null ? leftBrother : rightBrother;
+
+        if (borrow != rightBrother && rightBrother != null && rightBrother.getDataList().size() > 1) {
+            borrow = rightBrother;
+        }
+        T borrowData = null;
+        int parentIndex = -1;
+        Node<T> borrowNode = null;
+        if (borrow == leftBrother) {
+            borrowData = leftBrother.getMax();
+            parentIndex = parent.searchNodeIndex(node) - 1;
+            borrowNode = borrow.getRightNode();
+        } else {
+            borrowData = rightBrother.getMin();
+            parentIndex = parent.searchNodeIndex(node);
+            borrowNode = borrow.getLeftNode();
+        }
+        T parentData = parent.getDataList().get(parentIndex);
+        if (borrow.getDataList().size() > 1) {
+            node.add(parentData);
+            parent.removeData(parentData);
+            parent.add(borrowData);
+            borrow.removeData(borrowData);
+            if(borrowNode != null){
+                borrow.removeChildNode(borrowNode);
+                node.addChildNode(borrowNode);
+            }
+        } else {
+            borrow.add(parentData);
+            if (node.getChildrenList().size() > 0) {
+                for (Node<T> tNode : node.getChildrenList()) {
+                    borrow.addChildNode(tNode);
+                }
+            }
+            parent.removeChildNode(node);
+            if (parent.getDataList().size() == 1 && parent != root) {
+                down(parent);
+            }
+            if (parent == root && root.getChildrenList().size() == 1) {
+                root = borrow;
+            }
+            parent.removeData(parentData);
+        }
+    }
+
+    public static void main(String[] args) {
+        BTree<Integer> bTree = new BTree<Integer>(4);
+        Set<Integer> set = new HashSet<>();
+        List<Integer> list = new ArrayList<>();
+        Random r = new Random();
+        int count = 10000; // 添加多少个随机数
+        int max = 20000; // 随机数的范围
+        for (int i = 0; i < count; ) {
+            int k = r.nextInt(max);
+            if (set.add(k)) {
+                bTree.add(k);
+                list.add(k);
+                i++;
+            }
+        }
+        System.out.println("list:"+list);
+        System.out.println("-------------------");
+        bTree.printTree();
+        count = 0;
+        // 测试删除后留多少个
+        int l = 10;
+        for (Integer i : set) {
+            if (set.size()-count<=l) {
+                break;
+            }
+            bTree.delete(i);
+            System.out.println("删除了："+i);
+            bTree.printTree();
+            count++;
+        }
+    }
+
+    /**
+     * 获取右兄弟节点
+     *
+     * @param node
+     * @return
+     */
+    public Node<T> getRightBrother(Node<T> node) {
+        Node<T> parent = searchParentNode(node);
+        if (parent != null) {
+            int index = parent.searchNodeIndex(node);
+            if (index < parent.getChildrenList().size() - 1) {
+                return parent.getChildrenList().get(index + 1);
+            }
+        }
+        return null;
+    }
+
+    /**
+     * 获取左兄弟节点
+     *
+     * @param node
+     * @return
+     */
+    public Node<T> getLeftBrother(Node<T> node) {
+        Node<T> parent = searchParentNode(node);
+        if (parent != null) {
+            int index = parent.searchNodeIndex(node);
+            if (index > 0) {
+                return parent.getChildrenList().get(index - 1);
+            }
+        }
+        return null;
+    }
+
+    public T search(T data) {
+        return doSearch(root, data);
+    }
+
+    public Node<T> getMin(Node<T> start) {
+        Node<T> result = start;
+        while (result.getLeftNode() != null) {
+            result = result.getLeftNode();
+        }
+        return result;
+    }
+
+    public Node<T> getMax(Node<T> start) {
+        Node<T> result = start;
+        while (result.getRightNode() != null) {
+            result = result.getRightNode();
+        }
+        return result;
+    }
+
+    /**
+     * 得到关键字的左节点
+     *
+     * @param data
+     * @return
+     */
+    public Node<T> getLeftNode(T data) {
+        Node<T> node = searchNode(data);
+        if (node != null) {
+            return node.getLeftNode(data);
+        }
+        return null;
+    }
+
+    /**
+     * 得到关键字的左节点
+     *
+     * @param data
+     * @return
+     */
+    public Node<T> getRightNode(T data) {
+        Node<T> node = searchNode(data);
+        if (node != null) {
+            return node.getRightNode(data);
+        }
+        return null;
+    }
+
+    /**
+     * 查找关键字所在的node，如果不存在返回null
+     *
+     * @param data
+     * @return
+     */
+    public Node<T> searchNode(T data) {
+        return doSearchNode(root, data);
+    }
+
+    /**
+     * 查找父节点，如果查找不到返回null
+     *
+     * @param node
+     * @return
+     */
+    public Node<T> searchParentNode(Node<T> node) {
+        return doSearchParentNode(root, node);
+    }
+
+    public Node<T> searchInsertNode(T data) {
+        return doSearchInsertNode(this.root, data);
     }
 
     /**
      * 查找节点，找不到返回null
+     *
      * @param start
      * @param data
      * @return
      */
-    public T doSearch(Node<T> start,T data) {
-        if(start == null){
+    public T doSearch(Node<T> start, T data) {
+        if (start == null) {
             return null;
         }
         T min = start.getMin();
@@ -80,8 +297,8 @@ public class BTree<T extends Comparable<T>> {
             }
         } else {
             int index = start.searchInsertIndex(data);
-            if(start.getDataList().size() != 0){
-                if(start.getDataList().get(index).compareTo(data) == 0){
+            if (start.getDataList().size() != 0) {
+                if (start.getDataList().get(index).compareTo(data) == 0) {
                     return start.getDataList().get(index);
                 }
             }
@@ -90,7 +307,7 @@ public class BTree<T extends Comparable<T>> {
                 return doSearch(searchNode, data);
             } else {
                 int i = start.searchIndex(data);
-                if(i != -1){
+                if (i != -1) {
                     return start.getDataList().get(i);
                 }
             }
@@ -98,64 +315,40 @@ public class BTree<T extends Comparable<T>> {
         return null;
     }
 
-    /**
-     * 查找父节点，如果查找不到返回null
-     * @param node
-     * @return
-     */
-    public Node<T> searchParentNode(Node<T> node) {
-        return doSearchParentNode(root, node);
-    }
-
-    public static void main(String[] args) {
-        BTree<Integer> bTree = new BTree<Integer>(3);
-        bTree.add(30);
-        bTree.add(65);
-        bTree.add(18);
-        bTree.add(45);
-        bTree.add(50);
-        bTree.add(80);
-        bTree.add(10);
-        bTree.add(23);
-        bTree.add(37);
-        bTree.add(40);
-        bTree.add(48);
-        bTree.add(59);
-        bTree.add(77);
-        bTree.add(95);
-
-        // {53, 139, 75, 49, 145, 36, 101}
-        /*bTree.add(53);
-        bTree.add(139);
-        bTree.add(75);
-        bTree.add(49);
-        bTree.add(145);
-        bTree.add(36);
-        bTree.add(101);*/
-        bTree.printTree();
-
-        System.out.println(bTree.search(30));
-        System.out.println(bTree.search(65));
-        System.out.println(bTree.search(18));
-        System.out.println(bTree.search(45));
-        System.out.println(bTree.search(50));
-        System.out.println(bTree.search(80));
-        System.out.println(bTree.search(10));
-        System.out.println(bTree.search(23));
-        System.out.println(bTree.search(37));
-        System.out.println(bTree.search(40));
-        System.out.println(bTree.search(48));
-        System.out.println(bTree.search(59));
-        System.out.println(bTree.search(77));
-        System.out.println(bTree.search(95));
-
-        System.out.println(bTree.search(345));
-        System.out.println(bTree.search(89));
-        System.out.println(bTree.search(78));
-    }
-
-    public Node<T> searchInsertNode(T data) {
-        return doSearchInsertNode(this.root, data);
+    private Node<T> doSearchNode(Node<T> start, T data) {
+        if (start == null) {
+            return null;
+        }
+        T min = start.getMin();
+        T max = start.getMax();
+        if (data.compareTo(min) < 0) {
+            Node<T> left = start.getLeftNode(min);
+            if (left != null) {
+                return doSearchNode(left, data);
+            }
+        } else if (data.compareTo(max) > 0) {
+            Node<T> right = start.getRightNode(max);
+            if (right != null) {
+                return doSearchNode(right, data);
+            }
+        } else {
+            int index = start.searchInsertIndex(data);
+            if (start.getDataList().size() != 0) {
+                if (start.getDataList().get(index).compareTo(data) == 0) {
+                    return start;
+                }
+            }
+            if (start.getChildrenList().size() != 0) {
+                Node<T> searchNode = start.getChildrenList().get(index);
+                return doSearchNode(searchNode, data);
+            } else {
+                int i = start.searchIndex(data);
+                if (i != -1) {
+                    return start;
+                }
+            }
+        }
+        return null;
     }
 
     private Node<T> doSearchParentNode(Node<T> start, Node<T> node) {
@@ -286,7 +479,7 @@ public class BTree<T extends Comparable<T>> {
         public Node<T> getRightNode() {
             T max = getMax();
             if (max != null) {
-                return getLeftNode(max);
+                return getRightNode(max);
             }
             return null;
         }
@@ -385,6 +578,14 @@ public class BTree<T extends Comparable<T>> {
             this.childrenList.remove(node);
         }
 
+        public T removeData(T data) {
+            int index = searchIndex(data);
+            if (index != -1) {
+                return this.getDataList().remove(index);
+            }
+            return null;
+        }
+
         public void addChildNode(Node<T> node) {
             int i = searchNodeInsertIndex(node.getDataList().get(0));
             this.childrenList.add(i, node);
@@ -414,6 +615,31 @@ public class BTree<T extends Comparable<T>> {
 
         public int searchInsertIndex(T data) {
             return doSearchInsertIndex(data, 0, dataList.size() - 1);
+        }
+
+        /**
+         * 查找节点的下标
+         *
+         * @param node
+         * @return
+         */
+        public int searchNodeIndex(Node<T> node) {
+            return doSearchNodeIndex(node, 0, childrenList.size() - 1);
+        }
+
+        private int doSearchNodeIndex(Node<T> node, int startIndex, int endIndex) {
+            if (startIndex > endIndex) {
+                return -1;
+            }
+            int middle = (startIndex + endIndex) / 2;
+            Node<T> middleData = childrenList.get(middle);
+            if (node.compareTo(middleData) < 0) {
+                return doSearchNodeIndex(node, startIndex, middle - 1);
+            } else if (node.compareTo(middleData) > 0) {
+                return doSearchNodeIndex(node, middle + 1, endIndex);
+            } else {
+                return middle;
+            }
         }
 
         /**
