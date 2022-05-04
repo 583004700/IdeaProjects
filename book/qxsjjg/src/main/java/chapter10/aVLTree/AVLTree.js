4b4607b99c992cd5068d76032e69371a5ec8bcc1
zhuwb <583004700@qq.com>
2022-05-04 16:23:16 +0800

    AVL树添加删除过程canvas可视化

---
+++ book/qxsjjg/src/main/java/chapter10/aVLTree/AVLTree.js
@@ -0,0 +1,362 @@
+class AVLTree {
+    // root
+
+    printTree() {
+        let map = {};
+        let first = [];
+        first.push(this.root);
+        map[1] = first;
+        let level = 1;
+        while (map[level]) {
+            let nodes = map[level];
+            let nextKey = level + 1;
+            for (let i = 0; i < nodes.length; i++) {
+                let node = nodes[i];
+                if (!map[nextKey] && (node.left != null || node.right != null)) {
+                    map[nextKey] = [];
+                }
+                let nextNodes = map[nextKey];
+                if (node.left != null) {
+                    nextNodes.push(node.left);
+                }
+                if (node.right != null) {
+                    nextNodes.push(node.right);
+                }
+            }
+            level++;
+        }
+        level = 1;
+        while (map[level]) {
+            let nodes = map[level];
+            let s = "";
+            for (let i = 0; i < nodes.length; i++) {
+                let node = nodes[i];
+                s += node.data + "  ";
+            }
+            console.log(s);
+            level++;
+        }
+    }
+
+    drawTree(ctx,startX,startY,startW) {
+        ctx.clearRect(0, 0, 100000, 100000);
+        if(this.root) {
+            ctx.beginPath();
+            this.drawNode(ctx, this.root, startX, startY, startW);
+        }
+    }
+
+    drawNode(ctx, node, x, y, w) {
+        ctx.save();
+        ctx.beginPath();
+        ctx.arc(x, y, 15, 0, 2 * Math.PI);
+        ctx.font = "15px 微软雅黑";
+        if(node.color) {
+            ctx.strokeStyle = node.color;
+        }else{
+            ctx.strokeStyle = "black";
+        }
+        ctx.strokeText(node.data, x - 10, y + 6);
+        ctx.stroke();
+        ctx.restore();
+        ctx.beginPath();
+        w = w / 2;
+        if (node.left) {
+            ctx.moveTo(x,y+13);
+            ctx.lineTo(x - w,y - 13 + 50);
+            ctx.stroke();
+            this.drawNode(ctx, node.left, x - w, y + 50, w);
+        }
+        if (node.right) {
+            ctx.moveTo(x,y+13);
+            ctx.lineTo(x + w,y - 13 + 50);
+            ctx.stroke();
+            this.drawNode(ctx, node.right, x + w, y + 50, w);
+        }
+    }
+
+    delete(data) {
+        let target = this.search(data);
+        let route = null;
+        if (target) {
+            let parent = this.searchParent(target.data);
+            if (target.left == null && target.right == null) {
+                if (data.compareTo(parent.left.data) === 0) {
+                    parent.left = null;
+                } else if (data.compareTo(parent.right.data) === 0) {
+                    parent.right = null;
+                }
+                route = parent;
+                while (route != null) {
+                    let balance = this.isBalance(route);
+                    if (balance != null) {
+                        this.balance(balance);
+                    }
+                    route = this.searchParent(route.data);
+                }
+            } else if (target.left != null) {
+                let leftMax = this.doSearchMax(target.left);
+                delete (leftMax.data);
+                target.data = leftMax.data;
+            } else {
+                let rightMin = this.doSearchMin(target.right);
+                this.delete(rightMin.data);
+                target.data = rightMin.data;
+            }
+        }
+    }
+
+    LL(node) {
+        let left = node.left;
+        let leftRight = left.right;
+        left.right = node;
+        node.left = leftRight;
+        let parent = this.searchParent(node.data);
+        if (parent != null) {
+            if (node === parent.left) {
+                parent.left = left;
+            } else if (node === parent.right) {
+                parent.right = left;
+            }
+        } else {
+            this.root = left;
+        }
+        return left;
+    }
+
+    RR(node) {
+        let right = node.right;
+        let rightLeft = right.left;
+        right.left = node;
+        node.right = rightLeft;
+        let parent = this.searchParent(node.data);
+        if (parent != null) {
+            if (node === parent.left) {
+                parent.left = right;
+            } else if (node === parent.right) {
+                parent.right = right;
+            }
+        } else {
+            this.root = right;
+        }
+        return right;
+    }
+
+    searchParent(data) {
+        let t = new Node();
+        t.data = data;
+        return this.doSearchParent(this.root, t);
+    }
+
+    doSearchParent(start, node) {
+        if (start == null || node == null || node.data == null) {
+            return null;
+        }
+        let result = null;
+        if (start.left != null) {
+            if (node.data.compareTo(start.left.data) === 0) {
+                return start;
+            }
+            result = this.doSearchParent(start.left, node);
+            if (result != null) {
+                return result;
+            }
+        }
+        if (start.right != null) {
+            if (node.data.compareTo(start.right.data) === 0) {
+                return start;
+            }
+            return this.doSearchParent(start.right, node);
+        }
+        return null;
+    }
+
+    getHeight(){
+        return this.doHeight(this.root);
+    }
+
+    /**
+     * 获取树的高度
+     * @param node
+     * @returns {number}
+     */
+    doHeight(node) {
+        if (!node) {
+            return 0;
+        }
+        if (node.left == null && node.right == null) {
+            return 1;
+        }
+        let leftHeight = this.doHeight(node.left);
+        let rightHeight = this.doHeight(node.right);
+        let max = Math.max(leftHeight, rightHeight);
+        return max + 1;
+    }
+
+    /**
+     * 查找最小的节点
+     * @param node
+     */
+    doSearchMin(node) {
+        if (!node) {
+            return null;
+        }
+        let min = node;
+        while (min.left) {
+            min = node.left;
+        }
+        return min;
+    }
+
+    /**
+     * 查找最大的节点
+     * @param node
+     */
+    doSearchMax(node) {
+        if (!node) {
+            return null;
+        }
+        let max = node;
+        while (max.right) {
+            max = node.right;
+        }
+        return max;
+    }
+
+    /**
+     * 查找节点
+     * @param data
+     */
+    search(data) {
+        return this.doSearch(this.root, data);
+    }
+
+    doSearch(node, data) {
+        if (!node) {
+            return null;
+        }
+        if (data.compareTo(node.data) < 0) {
+            if (node.left != null) {
+                return this.doSearch(node.left, data);
+            } else {
+                return null;
+            }
+        } else if (data.compareTo(node.data) > 0) {
+            if (node.right != null) {
+                return this.doSearch(node.right, data);
+            } else {
+                return null;
+            }
+        } else {
+            return node;
+        }
+    }
+
+    add(data) {
+        this.doAdd(this.root, data);
+    }
+
+    doAdd(node, data) {
+        if (this.root == null) {
+            let temp = new Node();
+            temp.data = data;
+            this.root = temp;
+        } else {
+            let temp = null;
+            if (data.compareTo(node.data) < 0) {
+                if (node.left != null) {
+                    this.doAdd(node.left, data);
+                } else {
+                    temp = new Node();
+                    temp.data = data;
+                    node.left = temp;
+                }
+            } else if (data.compareTo(node.data) > 0) {
+                if (node.right != null) {
+                    this.doAdd(node.right, data);
+                } else {
+                    temp = new Node();
+                    temp.data = data;
+                    node.right = temp;
+                }
+            }
+            if (temp != null) {
+                let parent = null;
+                while ((parent = this.searchParent(temp.data)) != null) {
+                    let leftHeight = this.doHeight(parent.left);
+                    let rightHeight = this.doHeight(parent.right);
+                    if (Math.abs(leftHeight - rightHeight) > 1) {
+                        break;
+                    }
+                    temp = parent;
+                }
+                if (parent != null) {
+                    let balance = this.isBalance(temp);
+                    this.balance(balance);
+                }
+            }
+        }
+    }
+
+    balance(balance) {
+        let type = balance.type;
+        let parent = balance.node;
+        if ("LL" === type) {
+            this.LL(parent);
+        } else if ("RR" === type) {
+            this.RR(parent);
+        } else if ("LR" === type) {
+            this.RR(parent.left);
+            this.LL(parent);
+        } else if ("RL" === type) {
+            this.LL(parent.right);
+            this.RR(parent);
+        }
+    }
+
+    isBalance(temp) {
+        if (temp != null) {
+            let first = null;
+            let second = null;
+            let current = temp;
+            while (current != null) {
+                let leftHeight = this.doHeight(current.left);
+                let rightHeight = this.doHeight(current.right);
+                if (Math.abs(leftHeight - rightHeight) > 1) {
+                    break;
+                }
+                current = this.searchParent(current.data);
+            }
+            if (current != null) {
+                let leftHeight = this.doHeight(current.left);
+                let rightHeight = this.doHeight(current.right);
+                if (leftHeight < rightHeight) {
+                    first = "R";
+                    let secondLeftHeight = this.doHeight(current.right.left);
+                    let secondRightHeight = this.doHeight(current.right.right);
+                    if (secondLeftHeight < secondRightHeight) {
+                        second = "R";
+                    } else {
+                        second = "L";
+                    }
+                } else {
+                    first = "L";
+                    let secondLeftHeight = this.doHeight(current.left.left);
+                    let secondRightHeight = this.doHeight(current.left.right);
+                    if (secondLeftHeight < secondRightHeight) {
+                        second = "R";
+                    } else {
+                        second = "L";
+                    }
+                }
+                let type = first + second;
+                console.log("不平衡点为：" + current.data + ",类型为：" + type);
+                let balance = {};
+                balance.type = type;
+                balance.node = current;
+                return balance;
+            }
+        }
+        return null;
+    }
+}
---
+++ book/qxsjjg/src/main/java/chapter10/aVLTree/Node.js
@@ -0,0 +1,12 @@
+class Node {
+
+    // color,data,left,right
+
+    setRed(){
+        this.color = "red";
+    }
+
+    setBlack(){
+        this.color = "black";
+    }
+}
