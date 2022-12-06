4c77c42db0ba484095680b67491bf16cfd160306
zhuwb <583004700@qq.com>
2022-12-06 10:31:43 +0800

    gateway

--- sgg/springcloud-consul/springcloud-consul-dept-provider/src/main/java/com/demo/mydemo/config/Knife4jConfiguration.java
+++ sgg/springcloud-consul/springcloud-consul-dept-provider/src/main/java/com/demo/mydemo/config/Knife4jConfiguration.java
@@ -24,7 +24,7 @@ public class Knife4jConfiguration {
                         .version("1.0")
                         .build())
                 //分组名称
-                .groupName("student")
+                .groupName("dept")
                 .select()
                 //这里指定Controller扫描包路径
                 .apis(RequestHandlerSelectors.basePackage("com.demo.mydemo.controller"))
