34dc1112688ea6d740beb71485eb1d51a467a058
zhuwb <583004700@qq.com>
2023-04-08 18:24:58 +0800

    fund

--- sgg/project/fund/src/main/java/com/demo/mydemo/fund/FundApplication.java
+++ sgg/project/fund/src/main/java/com/demo/mydemo/fund/FundApplication.java
@@ -2,10 +2,21 @@ package com.demo.mydemo.fund;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class FundApplication {
     public static void main(String[] args) {
         SpringApplication.run(FundApplication.class, args);
+    }
+
+    @Bean
+    public ConfigurableServletWebServerFactory webServerFactory() {
+        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
+        factory.addConnectorCustomizers((TomcatConnectorCustomizer) connector -> connector.setProperty("relaxedQueryChars", "|{}[]\\"));
+        return factory;
     }
 }
