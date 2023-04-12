8b24b1386645fe2bb112e10221916f0d7088ce90
zhuwb <583004700@qq.com>
2023-04-12 10:49:42 +0800

    fund

--- sgg/project/fund/src/main/java/com/demo/mydemo/fund/task/FundTask.java
+++ sgg/project/fund/src/main/java/com/demo/mydemo/fund/task/FundTask.java
@@ -4,6 +4,8 @@ import com.demo.mydemo.fund.service.FundService;
 import com.demo.mydemo.fund.utils.DateUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
@@ -11,17 +13,19 @@ import java.util.Date;
 
 @Configuration
 @EnableScheduling
+@EnableAsync
 public class FundTask {
 
     @Autowired
     FundService fundService;
 
     @Scheduled(cron = "0 20 10 ? * MON-FRI")
-    private void insertBatchHistory() {
+    @Async
+    public void insertBatchHistory() {
         Date currentDate = new Date();
-        System.out.println("insertBatchHistory定时任务启动：当前时间为："+DateUtil.format(DateUtil.yyyy_MM_dd_HH_mm,currentDate));
+        System.out.println("insertBatchHistory定时任务启动：当前时间为：" + DateUtil.format(DateUtil.yyyy_MM_dd_HH_mm, currentDate));
         int row = fundService.insertBatchHistory(DateUtil.subDate(currentDate, 1));
         System.out.println("insertBatchHistory定时任务完成：当前时间为："
-                +DateUtil.format(DateUtil.yyyy_MM_dd_HH_mm,new Date())+";row为："+row);
+                + DateUtil.format(DateUtil.yyyy_MM_dd_HH_mm, new Date()) + ";row为：" + row);
     }
 }
