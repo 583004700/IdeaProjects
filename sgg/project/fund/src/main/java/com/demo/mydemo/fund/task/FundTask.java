4252afe7d46e8c93e08de26a6ec78cc3aa789fda
zhuwb <583004700@qq.com>
2023-04-14 19:07:16 +0800

    fund

--- sgg/project/fund/src/main/java/com/demo/mydemo/fund/task/FundTask.java
+++ sgg/project/fund/src/main/java/com/demo/mydemo/fund/task/FundTask.java
@@ -2,13 +2,12 @@ package com.demo.mydemo.fund.task;
 
 import com.demo.mydemo.fund.service.FundService;
 import com.demo.mydemo.fund.utils.DateUtil;
+import com.xxl.job.core.context.XxlJobHelper;
+import com.xxl.job.core.handler.annotation.XxlJob;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
 
 import java.util.Date;
 
@@ -20,7 +19,7 @@ public class FundTask {
     @Autowired
     FundService fundService;
 
-    @Scheduled(cron = "0 20 10 ? * MON-FRI")
+    /*@Scheduled(cron = "0 40 16 ? * MON-FRI")
     @Async
     @Lazy(value = false)
     public void insertBatchHistory() {
@@ -29,5 +28,15 @@ public class FundTask {
         int row = fundService.insertBatchHistory(DateUtil.subDate(currentDate, 1));
         System.out.println("insertBatchHistory定时任务完成：当前时间为："
                 + DateUtil.format(DateUtil.yyyy_MM_dd_HH_mm, new Date()) + ";row为：" + row);
+    }*/
+
+    @XxlJob("fundJobHandler")
+    public void fundJobHandler() {
+        Date currentDate = new Date();
+        XxlJobHelper.log("insertBatchHistory定时任务启动：当前时间为：" + DateUtil.format(DateUtil.yyyy_MM_dd_HH_mm, currentDate));
+        int row = fundService.insertBatchHistory(DateUtil.subDate(currentDate, 1));
+        XxlJobHelper.log("insertBatchHistory定时任务完成：当前时间为："
+                + DateUtil.format(DateUtil.yyyy_MM_dd_HH_mm, new Date()) + ";row为：" + row);
     }
+
 }
