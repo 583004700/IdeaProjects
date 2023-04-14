35f420810a48d5131f4b08ee89a1131451bbbe27
zhuwb <583004700@qq.com>
2023-04-14 12:40:37 +0800

    fund

---
+++ sgg/project/fund/src/main/java/com/demo/mydemo/fund/config/ScheduledTaskConfiguration.java
@@ -0,0 +1,17 @@
+package com.demo.mydemo.fund.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+
+@Configuration
+public class ScheduledTaskConfiguration implements SchedulingConfigurer {
+    @Override
+    public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
+        final ThreadPoolTaskScheduler taskScheduler = new ThreadPoolTaskScheduler();
+        taskScheduler.setPoolSize(8);
+        taskScheduler.initialize();
+        taskRegistrar.setTaskScheduler(taskScheduler);
+    }
+}
