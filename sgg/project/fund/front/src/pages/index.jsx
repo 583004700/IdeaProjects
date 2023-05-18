e0d7ff15d75536063963ae8d7d4c56b5315c39c8
zhuwb <583004700@qq.com>
2023-05-18 18:19:01 +0800

    1

--- sgg/project/fund/front/src/pages/index.jsx
+++ sgg/project/fund/front/src/pages/index.jsx
@@ -3,45 +3,48 @@ import {connect} from "react-redux";
 import {CHANGE_SELECTED} from "../redux/constant/routersActionType";
 import Tabs, {TabItem} from '../components/tabs/Tabs';
 import SidebarMenus from "../components/sidebar-menus/SidebarMenus";
+import '../styles/base.scss'
 
 class Index extends Component {
 
   state = {
-    tabItems: []
+    tabItems: [],
   }
   tabs = null;
   sidebarMenus = null;
   allRoutersMap = new Map();
 
-  onMenuOpen = (menu)=>{
-    if(!menu.childrenIds || menu.childrenIds.length === 0) {
+  components = [];
+
+  onMenuOpen = (menu) => {
+    if (!menu.childrenIds || menu.childrenIds.length === 0) {
       let routerItem = this.allRoutersMap.get(menu.id).routerItem;
       let tabItem: TabItem = new TabItem();
       tabItem.id = routerItem.id;
       tabItem.name = routerItem.name;
-      if(!this.tabs.has(tabItem.id)) {
+      if (!this.tabs.has(tabItem.id)) {
         this.tabs.addTabItem(tabItem);
       }
       this.tabs.selectTabItem(tabItem.id);
     }
   }
 
-  onTabSelected = (tab)=>{
+  onTabSelected = (tab) => {
     console.log(tab);
     this.sidebarMenus.openMenu(tab.id);
   }
 
-  onTabClose = (tab)=>{
+  onTabClose = (tab) => {
     console.log(tab);
   }
 
-  parseRouter(routerItemObj){
-     this.allRoutersMap.set(routerItemObj.routerItem.id,routerItemObj);
-     if(routerItemObj.children){
-       for (const element of routerItemObj.children) {
-         this.parseRouter(element);
-       }
-     }
+  parseRouter(routerItemObj) {
+    this.allRoutersMap.set(routerItemObj.routerItem.id, routerItemObj);
+    if (routerItemObj.children) {
+      for (const element of routerItemObj.children) {
+        this.parseRouter(element);
+      }
+    }
   }
 
   componentDidMount() {
@@ -51,31 +54,61 @@ class Index extends Component {
       this.parseRouter(element);
     }
 
-    const l = ()=>{
-      let path = location.hash.replace("#","");
-      alert(path);
+    const l = () => {
+      let path = location.hash.replace("#", "");
+      //alert(path);
     }
 
     window.addEventListener("hashchange", l, false);
 
     window.addEventListener("load", l, false);
+
+    let lastNRise = require('./fund/lastNRise').default;
+    let lastNRiseComp = React.createElement(lastNRise, null, null);
+    let account = require('./system/account').default;
+    let accountComp = React.createElement(account, null, null);
+    this.components.push({h: true, c: lastNRiseComp});
+    this.components.push({h: false, c: accountComp});
+    this.setState({});
+    setTimeout(()=>{
+      this.components[0].h = false;
+      this.setState({});
+    },3000)
+
+    setTimeout(()=>{
+      this.components[0].h = true;
+      this.setState({});
+    },8000)
+
+    setTimeout(()=>{
+      this.components[0].h = false;
+      this.setState({});
+    },13000)
   }
 
   render() {
-    let c = require('./fund/lastNRise').default;
-    let comp = React.createElement(c,null,null);
     const {tabItems} = this.state;
     const {allRouters} = this.props.routers;
-    console.log(allRouters);
     return (
-      <div>
+      <div className="clearfix">
         <Tabs tabItems={tabItems} ref={c => {
           this.tabs = c;
         }} onSelected={this.onTabSelected} onClose={this.onTabClose}/>
 
-        <SidebarMenus ref={c=>this.sidebarMenus = c} onOpen={this.onMenuOpen}
-                      style={{height: document.body.clientHeight - 33,width: 200,float: 'left'}} allRouters={allRouters}/>
-        {comp}
+        <SidebarMenus ref={c => this.sidebarMenus = c} onOpen={this.onMenuOpen}
+                      style={{height: document.body.clientHeight - 33, width: 200, float: 'left'}}
+                      allRouters={allRouters}/>
+        <div style={{float: "left"}}>
+          {
+            this.components.map(t => {
+              return (
+                <div style={{display: !t.h ? "block" : "none"}}>
+                  {t.c}
+                </div>
+              )
+            })
+          }
+        </div>
       </div>
     )
   }
---
+++ sgg/project/fund/front/src/pages/system/account.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+
+class Account extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Account</h1>
+        <input type={"text"}/>
+      </div>
+    )
+  }
+}
+
+export default Account;
